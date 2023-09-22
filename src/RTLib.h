4513bfb7ef2ecbc62a1c7822635eb402bda9484f
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2023-09-22 04:39:07 -0500

    Rendering shapes

---
+++ src/RTLib.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <glm.hpp>
+#include "types.h"
+
+//using namespace glm;
+
+#define EPSILON 0.00001f
+
+class HitInfo
+{
+public:
+    F32 t;
+    bool hit;
+    glm::vec3 hitLocation;
+    glm::vec3 normal;
+    glm::vec3 color;
+    bool emissive;
+
+    HitInfo() : t(0.f), hit(false), hitLocation(0.f), normal(0.f), color(0.f), emissive(false) {}
+};
+
+class Ray
+{
+public:
+    glm::vec3 origin;
+    glm::vec3 direction;
+
+    Ray() : origin(0.f), direction(0.f) {}
+};
+
+class Object
+{
+public:
+    glm::vec3 color;
+    bool emissive;
+
+    virtual HitInfo hit(Ray ray) const = 0;
+};
+
+class Triangle : public Object
+{
+public:
+    glm::vec3 v1, v2, v3;
+
+    Triangle() : v1(0.f), v2(0.f), v3(0.f) {}
+
+    HitInfo hit(Ray ray) const override;
+};
+
+class Sphere : public Object
+{
+public:
+    glm::vec3 center;
+    F32 radius;
+
+    Sphere() : radius(0.f), center(0.f) {}
+
+    HitInfo hit(Ray ray) const override;
+};
+
+extern F32 random();
+extern glm::vec3 randomVector(); // Get random vector in hemisphere.
+extern glm::vec3 randomDirection(glm::vec3 normal);
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,53 @@
 #include <SFML/Graphics.hpp>
 #include "types.h"
+#include "RTLib.h"
 
 #define WINDOW_WIDTH 400
 #define WINDOW_HEIGHT 400
 
+const glm::vec3 eye = glm::vec3(0.f, 0.f, 5.f);
+
 S32 main()
 {
+    std::vector<Object*> objects;
+
+    Sphere sphere;
+    sphere.center = glm::vec3(0.0f, -0.7f, -0.5f);
+    sphere.radius = 0.3f;
+    sphere.color = glm::vec3(2.f);
+    objects.push_back(&sphere);
+
+    Triangle floor1 = Triangle();
+    floor1.v1 = glm::vec3(1.f, -1.f, 1.f);
+    floor1.v2 = glm::vec3(1.f, -1.f, -1.f);
+    floor1.v3 = glm::vec3(-1.f, -1.f, -1.f);
+    floor1.color = glm::vec3(1.f);
+    objects.push_back(&floor1);
+
+    Triangle floor2 = Triangle();
+    floor2.v1 = glm::vec3(1.f, -1.f, 1.f);
+    floor2.v2 = glm::vec3(-1.f, -1.f, -1.f);
+    floor2.v3 = glm::vec3(-1.f, -1.f, 1.f);
+    floor2.color = glm::vec3(1.f);
+    objects.push_back(&floor2);
+
+    Triangle light1 = Triangle();
+    light1.v1 = glm::vec3(0.5f, 0.99f, 0.5f);
+    light1.v2 = glm::vec3(-0.5f, 0.99f, -0.5f);
+    light1.v3 = glm::vec3(-0.5f, 0.99f, 0.5f);
+    light1.color = glm::vec3(1.f);
+    objects.push_back(&light1);
+
+    Triangle light2 = Triangle();
+    light2.v1 = glm::vec3(0.5f, 0.99f, 0.5f);
+    light2.v2 = glm::vec3(0.5f, 0.99f, -0.5f);
+    light2.v3 = glm::vec3(-0.5f, 0.99f, -0.5f);
+    light2.color = glm::vec3(1.f);
+    objects.push_back(&light2);
+
     F64* image = new F64[WINDOW_WIDTH * WINDOW_HEIGHT * 3];
     memset(image, 0.0, sizeof(F64) * WINDOW_WIDTH * WINDOW_HEIGHT * 3);
+    F64* p = image;
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Ray Tracer");
 
@@ -20,9 +60,66 @@ S32 main()
     sf::Sprite sprite;
     sprite.setTexture(texture);
 
+    // Declaring variables here to prevent numerous re-declarations within while loop
+    HitInfo hi, finalHi;
+    Ray ray;
+    F32 i = 0.f, j = 0.f, minT = 10000000.f;
+    S32 r = 0, g = 0, b = 0;
+    glm::vec3 color = glm::vec3(0.f);
+
     while(window.isOpen())
     {
+        p = image;
+        for (S32 y = 0; y < WINDOW_HEIGHT; y++)
+        {
+            for (S32 x = 0; x < WINDOW_WIDTH; x++)
+            {
+                minT = 10000000.f;
+                finalHi.hit = false;
+                hi.hit = false;
+                color = glm::vec3(0.f);
 
+                i = (2.f * F32(x) / F32(WINDOW_WIDTH)) - 1.f;
+                j = (2.f * F32(WINDOW_HEIGHT - y) / F32(WINDOW_HEIGHT)) - 1.f;
+
+                ray.origin = glm::vec3(i, j, 1.2f);
+                ray.direction = glm::normalize(ray.origin - eye);
+
+                for (S32 k = 0; k < objects.size(); k++)
+                {
+                    hi = objects[k]->hit(ray);
+                    if (hi.hit && hi.t < minT)
+                    {
+                        finalHi = hi;
+                        minT = hi.t;
+                    }
+                }
+
+                if (finalHi.hit)
+                    color = finalHi.color;
+
+                *p += color.r;
+                p++;
+                *p += color.g;
+                p++;
+                *p += color.b;
+                p++;
+            }
+        }
+
+        p = image;
+
+        for (S32 yy = 0; yy < WINDOW_HEIGHT; yy++)
+        {
+            for (S32 xx = 0; xx < WINDOW_WIDTH; xx++)
+            {
+                r = glm::clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+                g = glm::clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+                b = glm::clamp(pow(*p++, 1.0f / 2.2f) * 255, 0.0, 255.0);
+
+                sfImage.setPixel(xx, yy, sf::Color(r, g, b));
+            }
+        }
 
         texture.loadFromImage(sfImage);
         sprite.setTexture(texture);
